3cbe2e346cac0daa0cf5fb34fa69ef019b66fb04
manmci <88697174+manmci@users.noreply.github.com>
2022-02-08 20:18:37 -0800

    basic CSS styling

--- php (RIP)/index.php
+++ php (RIP)/index.php
@@ -10,9 +10,9 @@
     <main>
         <img src="logo.svg" width="66%" alt="iHype!"><h1 class="invis">iHype!</h1>
         <h2>Tagline!</h2>
-        <div class="button"><a href="launch.php">Launch iHype</a></div>
-        <div class="button"><a href="about.php">Learn More</a></div>
-        <div class="button"><a href="team.php">The Team</a></div>
+        <button><a href="launch.php">Launch iHype</a></button>
+        <button><a href="about.php">Learn More</a></button>
+        <button><a href="team.php">The Team</a></button>
     </main>
 </body>
 <!-- End Content -->
--- php (RIP)/team.php
+++ php (RIP)/team.php
@@ -20,7 +20,7 @@
     <img src="img_placeholder.svg" class="TBD" alt="placeholder">
     <p class="name">Name</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <div class="button"><a href="launch.php">Launch iHype</a></div>
+    <button><a href="launch.php">Launch iHype</a></button>>
     </main>
 </body>
 <!-- End Content -->
