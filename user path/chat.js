cdd1188371f677ae49e5b8d545b72ac3cb893543
manmci <88697174+manmci@users.noreply.github.com>
2022-02-12 13:03:45 -0800

    bigass update

---
+++ user path/chat.js
@@ -0,0 +1,64 @@
+// HTML structure for UI Elements
+const userOpen = "<div class='left_align'><div class='chat_container user'><!-- top row --><div class='top_left'></div><div class='top_middle'></div><div class='top_right'></div><div class='break'></div><!-- content row --><div class='content_left'></div><div class='content_middle'>"
+
+const userClose = "</div><div class='content_right'></div><div class='break'></div><!--bottom row --><div class='bottom_left'></div><div class='bottom_middle'></div><div class='bottom_right'></div></div></div>"
+
+const botOpen = "<div class='right_align'><div class='chat_container bot'><!-- top row --><div class='top_left'></div><div class='top_middle'></div><div class='top_right'></div><div class='break'></div><!-- content row --><div class='content_left'></div><div class='content_middle'></div>"
+
+const botClose = "</div><div class='content_right'></div><div class='break'></div><!--bottom row --><div class='bottom_left'></div><div class='bottom_middle'></div><div class='bottom_right'></div></div></div>"
+
+const buttonOpen = "<div class='chat_button'><div class='top_left'></div><div class='top_middle'></div><div class='top_right'></div><div class='break'></div><!-- content row --><div class='content_left'></div><div class='content_middle'>"
+
+const buttonClose = "</div><div class='content_right'></div><div class='break'></div><!--bottom row --><div class='bottom_left'></div><div class='bottom_middle'></div><div class='bottom_right'></div></div>"
+
+// App Script
+    // Default opener is hard-coded
+    // User responses
+const doinOk = "Doin okay"
+
+const awesome = "Awesome!"
+
+const notGreat = "Not great"
+
+const hangin = "It's hangin&hellip;"
+
+    // iHype reply
+const word = "Word. Got any plans tonight?"
+
+    // User responses
+const excited = "Yeah, I'm excited!"
+
+const yeah = "Yeah&hellip;"
+
+const notYet = "Not yet."
+
+const nope = "Nope."
+
+    // iHype reply
+const stoked = "You don't sound so stoked. What's up?"
+
+    // User responses
+const poopEmoji = "I'm having a 💩 day"
+
+const notHot = "I'm not feelin so hot"
+
+    // iHype reply
+const sucks = "That sucks. Do you think seeing friends would cheer you up?"
+
+    // User responses
+const thinkSo = "Yeah, I think so"
+
+const dontKnow = "I don't know&hellip;"
+
+    // iHype reply
+const havePlans = "Do you have plans with your friends?"
+
+    // User responses
+const iDo = "I do, actually"
+
+const iDont = "I don't"
+
+    // iHype reply
+const seeFriends = "Go see your friends. It'll probably cheer you up."
+
+// Functions
