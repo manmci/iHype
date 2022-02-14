3c8ed6972ef5125b664f1fecb4f3b9101277ac71
manmci <88697174+manmci@users.noreply.github.com>
2022-02-13 21:38:48 -0800

    userflow

--- user path/chat.js
+++ user path/chat.js
@@ -1,7 +1,7 @@
 console.log('anystring')
 
 // HTML structure for UI Elements
-const userOpen = "<div class='left_align'><div class='chat_container user'><!-- top row --><div class='top_left'></div><div class='top_middle'></div><div class='top_right'></div><div class='break'></div><!-- content row --><div class='content_left'></div><div class='content_middle'>"
+const userOpen = "<div class='left_align'><div class='chat_container user'><!-- top row --><div class='top_left'></div><div class='top_middle'></div><div class='top_right'></div><div class='break'></div><!-- content row --><div class='content_left'></div>"
 
 const userClose = "</div><div class='content_right'></div><div class='break'></div><!--bottom row --><div class='bottom_left'></div><div class='bottom_middle'></div><div class='bottom_right'></div></div></div>"
 
@@ -63,6 +63,9 @@ const iDont = "<div class='content_middle' id='iDont'>I don't"
     // iHype reply
 const seeFriends = "Go see your friends. It'll probably cheer you up."
 
+    //End of demo
+const end = "<div class='content_middle' id='end'>End of protoHype. Thank you for trying iHype!"
+
 // Functions
 
 // Default opener is hard-coded
@@ -72,15 +75,64 @@ const seeFriends = "Go see your friends. It'll probably cheer you up."
         $("footer").append(buttonOpen + awesome + buttonClose);
         $("footer").append(buttonOpen + notGreat + buttonClose);
         $("footer").append(buttonOpen + hangin + buttonClose);
-        $(".chat_button").fadeIn("slow", function(){});
-    });
+        $("#doinOK").css("color", "#5533BB");
+        $("#awesome").css("color", "#5533BB");
+        $("#notGreat").css("color", "#5533BB");
+// User selects response            
+        $("#hangin").on("click", function (){
+// Footer is cleared, user input added to chat
+        $("footer").empty();
+        $("main").append(userOpen + hangin + userClose).scrollTop($("main")[0].scrollHeight);
+// "Bot" response & options added
+            $("main").append(botOpen + word + botClose).scrollTop($("main")[0].scrollHeight);
+            $("footer").append(buttonOpen + excited +buttonClose);
+            $("footer").append(buttonOpen + yeah +buttonClose);
+            $("footer").append(buttonOpen + notYet +buttonClose);
+            $("footer").append(buttonOpen + nope +buttonClose);
+            $("#excited").css("color", "#5533BB");
+            $("#notYet").css("color", "#5533BB");
+            $("#nope").css("color", "#5533BB");
 // User selects response
-$("#hangin").on("click", function (){
-    console.log("why!!!");
-});
-
-    // $("#hangin").on("click", function (){
-    //     console.log('poke');
-    //     $("main").append(buttonOpen + hangin + buttonClose);
-    //     $("main").scrollTop($("main")[0].scrollHeight);
-    // });
+            $("#yeah").on("click", function (){
+// Footer is cleared, user input added
+            $("footer").empty();
+            $("main").append(userOpen + yeah + userClose).scrollTop($("main")[0].scrollHeight);
+// "Bot" response & options added 
+                $("main").append(botOpen + stoked + botClose).scrollTop($("main")[0].scrollHeight);
+                $("footer").append(buttonOpen + poopEmoji +buttonClose);
+                $("footer").append(buttonOpen + notHot +buttonClose);
+                $("#poopEmoji").css("color", "#5533BB");
+// User selects response
+                $("#notHot").on("click", function(){
+// Footer is cleared, user input added
+                    $("footer").empty();
+                    $("main").append(userOpen + notHot + userClose).scrollTop($("main")[0].scrollHeight);
+// "Bot" response & options added 
+                    $("main").append(botOpen + sucks + botClose).scrollTop($("main")[0].scrollHeight);
+                    $("footer").append(buttonOpen + thinkSo + buttonClose);
+                    $("footer").append(buttonOpen + dontKnow + buttonClose);
+                    $("#dontKnow").css("color", "#5533BB");
+// User selects response
+                    $("#thinkSo").on("click", function(){
+// Footer is cleared, user input added                        
+                        $("footer").empty();
+                        $("main").append(userOpen + thinkSo + userClose).scrollTop($("main")[0].scrollHeight);
+// "Bot" response & options added
+                        $("main").append(botOpen + havePlans + botClose).scrollTop($("main")[0].scrollHeight);
+                        $("footer").append(buttonOpen + iDo + buttonClose);
+                        $("footer").append(buttonOpen + iDont + buttonClose);
+                        $("#iDont").css("color", "#5533BB");
+// User selects response
+                        $("#iDo").on("click", function(){
+// Footer is cleared, user input added 
+                            $("footer").empty();
+                            $("main").append(userOpen + iDo + userClose).scrollTop($("main")[0].scrollHeight);
+// "Bot" response & demo ended
+                            $("main").append(botOpen + seeFriends + botClose).scrollTop($("main")[0].scrollHeight);
+                            $("footer").append(buttonOpen + end + buttonClose);
+                        });
+                    });                    
+                });
+            });                    
+        });
+    });
